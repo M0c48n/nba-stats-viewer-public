bc4bfe3f41cebfb6bc7283f05162c4c7c48b8f14
M0c48n <d41ki103@gmail.com>
2023-05-18 00:31:13 +0900

    コメントの最適化

--- src/player/views.py
+++ src/player/views.py
@@ -11,6 +11,7 @@ from player.models import Stats
 # Create your views here.
 logger = logging.getLogger(__name__)
 
+# RapidAPIを呼び出すためのヘッダ情報を設定
 headers = {
     "X-RapidAPI-Key": settings.X_RAPIDAPI_KEY,
     "X-RapidAPI-Host": settings.X_RAPIDAPI_HOST,
@@ -18,30 +19,33 @@ headers = {
 
 
 def stats(request, player_id):
-    # statsを返却する関数
-    # 選択したプレイヤーの名前を取得
+    """
+    選択したプレイヤーのスタッツ情報を表示するビュー関数
+    """
+    # RapidAPIから選択したプレイヤーの名前を取得
     try:
         url = f'https://api-nba-v1.p.rapidapi.com/players?id={player_id}'
         current_player = call_api(url)
     except Exception as e:
         logger.error(f'選手情報の取得に失敗しました: {e}')
         return HttpResponseServerError()
-
+    # プレイヤーの名前を取得
     plyer_name = f"{current_player['response'][0]['firstname']} {current_player['response'][0]['lastname']}"
 
-    # 最新のシーズンを取得する
+    # RapidAPIからシーズン情報を取得
     try:
         url = f'https://api-nba-v1.p.rapidapi.com/seasons'
         seasons = call_api(url)
     except Exception as e:
         logger.error(f'シーズン情報の取得に失敗しました: {e}')
         return HttpResponseServerError()
-    
+    # 最新のシーズンを取得
     latest_season = seasons['response'][-1]
-    # 返却値に必要なデータをDBから取得
+
+    # DBから選択したプレイヤーの最新シーズンのstatsを取得
     db_stats = Stats.objects.select_related('game').filter(
         player_id=player_id, game__season=latest_season)
-    # DBデータからstatsのリストを作成する
+    # 取得したstatsから表示用のリストを作成
     stats_list = create_stats_list(db_stats)
 
     context = {'current_player_name': plyer_name, 'stats_list': stats_list}
@@ -50,7 +54,10 @@ def stats(request, player_id):
 
 
 def call_api(url):
-
+    """
+    指定されたURLにGETリクエストを送信し、レスポンスをJSON形式で返す
+    """
+    # リトライを設定したセッションを作成
     session = requests.Session()
     retries = requests.adapters.Retry(
         total=3, backoff_factor=1, status_forcelist=[429, 500, 502, 503, 504])
@@ -59,14 +66,20 @@ def call_api(url):
     try:
         # API呼び出し
         response = session.get(url, headers=headers, timeout=10)
+        # レスポンスがエラーだった場合に例外を発生させる
         response.raise_for_status()
+        # レスポンスボディをJSONとして解析
         response_data = response.json()
     except (requests.exceptions.RequestException, ValueError) as e:
+        # リクエストやレスポンスの処理中にエラーが発生した場合は例外を発生させる
         raise Exception(f'Error: {url} - {e}')
 
     return response_data
 
 def create_stats_list(db_stats):
+    """
+    statsオブジェクトのリストから表示用のstatsリストを作成
+    """
     return [
         {
             'date': s.game.date,
